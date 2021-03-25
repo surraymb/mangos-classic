1cb702f43362f491b6c7426de7d1cfc954e91d75
celguar <celguar@gmail.com>
2021-03-25 22:33:02 +0300

    Fix null ptr crash

--- src/game/Entities/Unit.cpp
+++ src/game/Entities/Unit.cpp
@@ -7538,7 +7538,7 @@ void Unit::SetInCombatState(bool PvP, Unit* enemy)
                     if (PvP || creatureNotInCombat)
                         enemy->GetCombatManager().TriggerCombatTimer(controller);
                 }
-                else
+                else if (controller->AI())
                     controller->AI()->AttackStart(enemy);
             }
         }
