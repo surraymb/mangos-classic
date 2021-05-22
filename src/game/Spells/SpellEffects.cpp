945433660cd11870d49c7a48b9c5a5d172182438
celguar <celguar@gmail.com>
2021-05-23 00:53:35 +0300

    Additional ike3 bots mods
    Remove Wait for Ress on revive
    Fix Teleport notify (from vmangos)
    Get BG object function from vmangos
    Fix lfg channel flag
    
    Additional ike3 bots mods
    
    Remove Wait for Ress on revive
    Fix Teleport notify (from vmangos)
    Get BG object function from vmangos
    Fix lfg channel flag
    
    Update Bots

--- src/game/Spells/SpellEffects.cpp
+++ src/game/Spells/SpellEffects.cpp
@@ -5745,6 +5745,7 @@ void Spell::EffectSpiritHeal(SpellEffectIndex /*eff_idx*/)
 
     if (Player* player = static_cast<Player*>(unitTarget))
     {
+        player->RemoveAurasDueToSpell(2584);
         player->ResurrectPlayer(1.0f);
         player->SpawnCorpseBones();
 
