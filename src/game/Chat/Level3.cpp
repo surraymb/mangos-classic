4d7c1b02457154d4de8a114d9d87803d4e4be49d
celguar <celguar@gmail.com>
2022-02-01 23:54:47 +0300

    Custom stuff
    Save some info into armory feed
    
    Includes bosses killed, items
    
    Save more stats to database
    
    Instant taxi option
    
    Option to make players on taxi visible from afar
    
    Add RU Locale support
    
    Solocraft port by niam
    
    Fix Seal of Righteousness damage
    
    Fix DynamicObjects collision height not defined, causes AoE like blizzard not work in most cases
    
    Handle extra attack in Unit::Update
    
    Fix Reckoning talent (port from vmangos)
    
    Fix visibility of far objects
    
    Set door/flags to have maximum distance
    Fix "large" objects getting lower visibility (200y) than BG (533y)
    
    Fix necropolis visibility
    
    Fix scourge invasion spam mail every server start
    
    Fix reset instances menu availability
    
    AQ effort enable/disable config
    
    AQ effort rates config
    Fix aq gates state change
    
    Fix trainer list showing red spells as green
    
    Use SMSG_TRAINER_BUY_FAILED on failed trainer buy
    
    Fix ready check
    
    Fix aq effort rates
    
    Fix far objects visibility
    
    Show areatrigger message if condition is not met
    
    AQ gates condition support
    
    Fix merge

--- src/game/Chat/Level3.cpp
+++ src/game/Chat/Level3.cpp
@@ -2479,7 +2479,7 @@ bool ChatHandler::HandleLookupItemSetCommand(char* args)
             if (!Utf8FitTo(name, wnamepart))
             {
                 loc = 0;
-                for (; loc < MAX_LOCALE; ++loc)
+                for (; loc < MAX_DBC_LOCALE; ++loc)
                 {
                     if (loc == GetSessionDbcLocale())
                         continue;
@@ -2493,7 +2493,7 @@ bool ChatHandler::HandleLookupItemSetCommand(char* args)
                 }
             }
 
-            if (loc < MAX_LOCALE)
+            if (loc < MAX_DBC_LOCALE)
             {
                 // send item set in "id - [namedlink locale]" format
                 if (m_session)
@@ -2542,7 +2542,7 @@ bool ChatHandler::HandleLookupSkillCommand(char* args)
             if (!Utf8FitTo(name, wnamepart))
             {
                 loc = 0;
-                for (; loc < MAX_LOCALE; ++loc)
+                for (; loc < MAX_DBC_LOCALE; ++loc)
                 {
                     if (loc == GetSessionDbcLocale())
                         continue;
@@ -2556,7 +2556,7 @@ bool ChatHandler::HandleLookupSkillCommand(char* args)
                 }
             }
 
-            if (loc < MAX_LOCALE)
+            if (loc < MAX_DBC_LOCALE)
             {
                 char valStr[50] = "";
                 char const* knownStr = "";
@@ -2667,7 +2667,7 @@ bool ChatHandler::HandleLookupSpellCommand(char* args)
             if (!Utf8FitTo(name, wnamepart))
             {
                 loc = 0;
-                for (; loc < MAX_LOCALE; ++loc)
+                for (; loc < MAX_DBC_LOCALE; ++loc)
                 {
                     if (loc == GetSessionDbcLocale())
                         continue;
@@ -2681,7 +2681,7 @@ bool ChatHandler::HandleLookupSpellCommand(char* args)
                 }
             }
 
-            if (loc < MAX_LOCALE)
+            if (loc < MAX_DBC_LOCALE)
             {
                 ShowSpellListHelper(target, spellInfo, LocaleConstant(loc));
                 ++counter;
@@ -2906,7 +2906,7 @@ bool ChatHandler::HandleLookupTaxiNodeCommand(char* args)
             if (!Utf8FitTo(name, wnamepart))
             {
                 loc = 0;
-                for (; loc < MAX_LOCALE; ++loc)
+                for (; loc < MAX_DBC_LOCALE; ++loc)
                 {
                     if (loc == GetSessionDbcLocale())
                         continue;
@@ -2920,7 +2920,7 @@ bool ChatHandler::HandleLookupTaxiNodeCommand(char* args)
                 }
             }
 
-            if (loc < MAX_LOCALE)
+            if (loc < MAX_DBC_LOCALE)
             {
                 // send taxinode in "id - [name] (Map:m X:x Y:y Z:z)" format
                 if (m_session)
@@ -6964,12 +6964,24 @@ bool ChatHandler::HandleVariablePrint(char* args)
 
 bool ChatHandler::HandleWarEffortCommand(char* args)
 {
+    if (!sWorld.getConfig(CONFIG_BOOL_WAREFFORT_ENABLE))
+    {
+        PSendSysMessage("War Effort is disabled in config!");
+        return true;
+    }
+
     PSendSysMessage("%s", sWorldState.GetAQPrintout().data());
     return true;
 }
 
 bool ChatHandler::HandleWarEffortPhaseCommand(char* args)
 {
+    if (!sWorld.getConfig(CONFIG_BOOL_WAREFFORT_ENABLE))
+    {
+        PSendSysMessage("War Effort is disabled in config!");
+        return true;
+    }
+
     uint32 param;
     if (!ExtractUInt32(&args, param))
     {
@@ -6980,8 +6992,37 @@ bool ChatHandler::HandleWarEffortPhaseCommand(char* args)
     return true;
 }
 
+bool ChatHandler::HandleWarEffortGateCommand(char* args)
+{
+    if (!sWorld.getConfig(CONFIG_BOOL_WAREFFORT_ENABLE))
+    {
+        PSendSysMessage("War Effort is disabled in config!");
+        return true;
+    }
+
+    uint32 param;
+    if (!ExtractUInt32(&args, param))
+    {
+        PSendSysMessage("Enter valid value. 1 to close AQ Gate, 0 to open");
+        return true;
+    }
+    else if (param == 0)
+        sWorldState.HandleWarEffortGateSwitch(false);
+    else if (param == 1)
+        sWorldState.HandleWarEffortGateSwitch(true);
+    else
+        PSendSysMessage("Enter valid value. 1 to close AQ Gate, 0 to open");
+    return true;
+}
+
 bool ChatHandler::HandleWarEffortCounterCommand(char* args)
 {
+    if (!sWorld.getConfig(CONFIG_BOOL_WAREFFORT_ENABLE))
+    {
+        PSendSysMessage("War Effort is disabled in config!");
+        return true;
+    }
+
     uint32 index;
     if (!ExtractUInt32(&args, index) || index >= RESOURCE_MAX)
     {
