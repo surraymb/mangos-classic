584549c36e3907ca8f3d18b8cca58ddc5b6ae0ea
celguar <celguar@gmail.com>
2022-07-28 14:58:39 +0300

    Transmog Mod (Thx Ace)

--- src/game/AI/ScriptDevAI/system/ScriptLoader.cpp
+++ src/game/AI/ScriptDevAI/system/ScriptLoader.cpp
@@ -8,6 +8,7 @@
 extern void AddSC_battleground();
 
 // custom
+extern void AddSC_Transmog();
 
 // examples
 extern void AddSC_example_creature();
@@ -213,6 +214,7 @@ void AddScripts()
     AddSC_battleground();
 
     // custom
+    AddSC_Transmog();
 
     // examples
     AddSC_example_creature();
