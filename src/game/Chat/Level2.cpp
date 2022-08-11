3c2b180232616ee90af4c8367d25a5c86ba73cf3
celguar <celguar@gmail.com>
2022-08-11 05:43:29 +0300

    City Protector rank port from vmangos

--- src/game/Chat/Level2.cpp
+++ src/game/Chat/Level2.cpp
@@ -3642,6 +3642,27 @@ bool ChatHandler::HandleCharacterReputationCommand(char* args)
     return true;
 }
 
+bool ChatHandler::HandleCharacterCityTitleCommand(char* args)
+{
+    Player* target;
+    if (!ExtractPlayerTarget(&args, &target))
+        return false;
+
+    bool value;
+    if (!ExtractOnOff(&args, value))
+    {
+        PSendSysMessage("Syntax: .title on | off");
+        return false;
+    }
+
+    if (value)
+        target->SetCityTitle();
+    else
+        target->RemoveCityTitle();
+
+    return true;
+}
+
 // change standstate
 bool ChatHandler::HandleModifyStandStateCommand(char* args)
 {
