9956c5fb305518f593ccab60ca21b0b0aedbce81
celguar <celguar@gmail.com>
2022-08-15 23:29:38 +0300

    Restore Collectors Edition
    command to enable collector's edition
    config to enable it for all by default

--- src/game/Server/WorldSession.h
+++ src/game/Server/WorldSession.h
@@ -146,6 +146,7 @@ enum AccountFlags
     ACCOUNT_FLAG_SILENCED       = 0x02,
     ACCOUNT_FLAG_SHOW_ANTISPAM  = 0x04,
     ACCOUNT_FLAG_HIDDEN         = 0x08,
+    ACCOUNT_FLAG_COLLECTOR      = 0x10,
 };
 
 // class to deal with packet processing
