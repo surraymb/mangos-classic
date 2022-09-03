36ad2f194b58632a7087617accd3f0e42ebbc599
celguar <celguar@gmail.com>
2022-09-04 02:49:36 +0300

    Learn all my level command

--- src/game/Chat/Chat.h
+++ src/game/Chat/Chat.h
@@ -387,6 +387,7 @@ class ChatHandler
         bool HandleLearnAllMyClassCommand(char* args);
         bool HandleLearnAllMySpellsCommand(char* args);
         bool HandleLearnAllMyTalentsCommand(char* args);
+        bool HandleLearnAllMyLevelCommand(char* args);
 
         bool HandleListAreaTriggerCommand(char* args);
         bool HandleListAurasCommand(char* args);
