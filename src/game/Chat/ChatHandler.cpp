1e4a35f4537fc415d6ccfdbfa58f03604cc6da58
mostlikely4r <sebastiaan.keek@gmail.com>
2022-11-08 15:48:17 +0100

    Pass language to HandleCommand for adding filtering

--- src/game/Chat/ChatHandler.cpp
+++ src/game/Chat/ChatHandler.cpp
@@ -265,7 +265,7 @@ void WorldSession::HandleMessagechatOpcode(WorldPacket& recv_data)
 #ifdef ENABLE_PLAYERBOTS
             if (player->GetPlayerbotAI() && lang != LANG_ADDON)
             {
-                player->GetPlayerbotAI()->HandleCommand(type, msg, *GetPlayer());
+                player->GetPlayerbotAI()->HandleCommand(type, msg, *GetPlayer(), lang);
                 GetPlayer()->m_speakTime = 0;
                 GetPlayer()->m_speakCount = 0;
             }
@@ -315,7 +315,7 @@ void WorldSession::HandleMessagechatOpcode(WorldPacket& recv_data)
                 Player* player = itr->getSource();
                 if (player && player->GetPlayerbotAI() && lang != LANG_ADDON)
                 {
-                    player->GetPlayerbotAI()->HandleCommand(type, msg, *GetPlayer());
+                    player->GetPlayerbotAI()->HandleCommand(type, msg, *GetPlayer(), lang);
                     GetPlayer()->m_speakTime = 0;
                     GetPlayer()->m_speakCount = 0;
                 }
@@ -354,7 +354,7 @@ void WorldSession::HandleMessagechatOpcode(WorldPacket& recv_data)
                 {
                     Player* const bot = it->second;
                     if (bot->GetGuildId() == GetPlayer()->GetGuildId() && lang != LANG_ADDON)
-                        bot->GetPlayerbotAI()->HandleCommand(type, msg, *GetPlayer());
+                        bot->GetPlayerbotAI()->HandleCommand(type, msg, *GetPlayer(), lang);
                 }
             }
 #endif
@@ -426,7 +426,7 @@ void WorldSession::HandleMessagechatOpcode(WorldPacket& recv_data)
                 Player* player = itr->getSource();
                 if (player && player->GetPlayerbotAI())
                 {
-                    player->GetPlayerbotAI()->HandleCommand(type, msg, *GetPlayer());
+                    player->GetPlayerbotAI()->HandleCommand(type, msg, *GetPlayer(), lang);
                     GetPlayer()->m_speakTime = 0;
                     GetPlayer()->m_speakCount = 0;
                 }
@@ -472,7 +472,7 @@ void WorldSession::HandleMessagechatOpcode(WorldPacket& recv_data)
                 Player* player = itr->getSource();
                 if (player && player->GetPlayerbotAI())
                 {
-                    player->GetPlayerbotAI()->HandleCommand(type, msg, *GetPlayer());
+                    player->GetPlayerbotAI()->HandleCommand(type, msg, *GetPlayer(), lang);
                     GetPlayer()->m_speakTime = 0;
                     GetPlayer()->m_speakCount = 0;
                 }
@@ -599,14 +599,14 @@ void WorldSession::HandleMessagechatOpcode(WorldPacket& recv_data)
 #ifdef ENABLE_PLAYERBOTS
                     // if GM apply to all random bots
                     if (GetSecurity() > SEC_PLAYER && GetPlayer()->IsGameMaster())
-                        sRandomPlayerbotMgr.HandleCommand(type, msg, *_player);
+                        sRandomPlayerbotMgr.HandleCommand(type, msg, *_player, "", TEAM_BOTH_ALLOWED, lang);
                     else
-                        sRandomPlayerbotMgr.HandleCommand(type, msg, *_player, "", GetPlayer()->GetTeam());
+                        sRandomPlayerbotMgr.HandleCommand(type, msg, *_player, "", GetPlayer()->GetTeam(), lang);
 
                     // apply to own bots
                     if (_player->GetPlayerbotMgr() && chn->GetFlags() & 0x18)
                     {
-                        _player->GetPlayerbotMgr()->HandleCommand(type, msg);
+                        _player->GetPlayerbotMgr()->HandleCommand(type, msg, lang);
                     }
 #endif
                 }
