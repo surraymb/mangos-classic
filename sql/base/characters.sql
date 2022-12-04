914f9f291df666fb339532981e22313db8e2f8aa
celguar <celguar@gmail.com>
2022-12-04 03:05:11 +0300

    Add missing sql mods

--- sql/base/characters.sql
+++ sql/base/characters.sql
@@ -1565,6 +1565,43 @@ CREATE TABLE IF NOT EXISTS `character_armory_feed` (
 
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
+-- Dumping structure for table tc_c.custom_transmogrification
+CREATE TABLE IF NOT EXISTS `custom_transmogrification` (
+  `GUID` int(10) unsigned NOT NULL COMMENT 'Item guidLow',
+  `FakeEntry` int(10) unsigned NOT NULL COMMENT 'Item entry',
+  `Owner` int(10) unsigned NOT NULL COMMENT 'Player guidLow',
+  PRIMARY KEY (`GUID`),
+  KEY `Owner` (`Owner`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='6_2';
+
+-- Data exporting was unselected.
+
+
+-- Dumping structure for table tc_c.custom_transmogrification_sets
+CREATE TABLE IF NOT EXISTS `custom_transmogrification_sets` (
+  `Owner` int(10) unsigned NOT NULL COMMENT 'Player guidlow',
+  `PresetID` tinyint(3) unsigned NOT NULL COMMENT 'Preset identifier',
+  `SetName` text COMMENT 'SetName',
+  `SetData` text COMMENT 'Slot1 Entry1 Slot2 Entry2',
+  PRIMARY KEY (`Owner`,`PresetID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='6_1';
+
+CREATE TABLE IF NOT EXISTS `custom_unlocked_appearances` (
+    `account_id`       int(10) unsigned      NOT NULL,
+    `item_template_id` mediumint(8) unsigned NOT NULL DEFAULT '0',
+    PRIMARY KEY (`account_id`, `item_template_id`)
+) ENGINE = InnoDB DEFAULT CHARSET = utf8;
+
+DROP TABLE IF EXISTS `custom_solocraft_character_stats`;
+CREATE TABLE `custom_solocraft_character_stats` (
+  `GUID` tinyint(3) unsigned NOT NULL,
+  `Difficulty` float NOT NULL,
+  `GroupSize` int(11) NOT NULL,
+  `SpellPower` int(10) unsigned NOT NULL DEFAULT '0',
+  `Stats` float NOT NULL DEFAULT '100',
+  PRIMARY KEY (`GUID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
 /*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
