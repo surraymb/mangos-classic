a63337e1508c988ed5b33f3b04c600edea6ac5b1
celguar <celguar@gmail.com>
2023-07-24 14:10:34 +0300

    Revert "Add login optimization for slow speed drives"
    This reverts commit d7154d5e9b1b6e34434fd88c1611fe2c654e5d59.

--- src/shared/Database/SqlOperations.h
+++ src/shared/Database/SqlOperations.h
@@ -131,7 +131,6 @@ class SqlQueryHolder
         QueryResult* GetResult(size_t index);
         void SetResult(size_t index, QueryResult* result);
         bool Execute(MaNGOS::IQueryCallback* callback, SqlDelayThread* thread, SqlResultQueue* queue);
-        bool ManualExecute(Database* db);
 };
 
 class SqlQueryHolderEx : public SqlOperation
